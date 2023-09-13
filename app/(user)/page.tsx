71d2e858cc59ae58001bf1417b8b14e7be86d10c
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-09-13 17:20:03 +0500

    Implement Sanity Preview

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,21 @@
 import Image from "next/image";
+import { draftMode } from "next/headers";
+import { groq } from "next-sanity";
 
 export default function Home() {
+  const preview = draftMode().isEnabled;
+  const query = groq`
+  *[_type=='post']{
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+  `;
+
+  if (preview) return <div>Preview Mode</div>;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Home
+    </main>
   );
 }
